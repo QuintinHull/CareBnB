5c477997f65304a855cbe5622a39a313acc8a671
Quintin Hull <quintinhull92@gmail.com>
2021-02-20 13:41:09 -0500

    spots model

---
+++ app/models/spot.py
@@ -0,0 +1,36 @@
+from .db import db
+
+
+class Spot(db.Model, UserMixin):
+    __tablename__ = 'spots'
+
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(20083), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    availability = db.Column(db.Integer, nullable=False)
+    host_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    host = db.relationship("User", back_populates="spots")
+
+    @property
+    def title(self):
+        return self.title
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "image_url": self.image_url,
+            "title": self.title,
+            "address": self.address
+            "city": self.city
+            "state": self.state
+            "description": self.description
+            "capacity": self.capacity
+            "availability": self.availability
+            "host_id": self.host_id
+        }
--- app/models/user.py
+++ app/models/user.py
@@ -12,6 +12,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    spots = db.relationship("Spot", back_populates="users")
+
     @property
     def password(self):
         return self.hashed_password
