eeee8a4274801f6e7948c9b6ef0a7a168a851098
Jummies <jumpark1210@gmail.com>
2021-02-22 16:10:24 -0500

    added spots post route

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -42,6 +42,7 @@ def login():
         # Add the user to the session, we are logged in!
         user = User.query.filter(User.email == form.data['email']).first()
         login_user(user)
+        print('current_user.id: ---------', current_user.id)
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
@@ -72,6 +73,7 @@ def sign_up():
         db.session.add(user)
         db.session.commit()
         login_user(user)
+
         return user.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}
 
--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
-
+from flask_login import current_user
 from app.models import Spot
+from app.forms import SpotForm
 
 spot_routes = Blueprint('spots', __name__)
 
@@ -8,7 +9,31 @@ spot_routes = Blueprint('spots', __name__)
 @spot_routes.route('/')
 def all_spots():
     spots = Spot.query.all()
+    print('current_user.id', current_user.id)
     return {"all_spots": {spot.id: spot.to_dict() for spot in spots}}
+
+
+@spot_routes.route('/', methods=["POST"])
+def create_spot():
+    form = SpotForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        spot = Spot(
+            image_url = form.data['image_url'],
+            title = form.data['title'],
+            address = form.data['address'],
+            city = form.data['city'],
+            state = form.data['state'],
+            zipcode = form.data['zipcode'],
+            description = form.data['description'],
+            capacity = form.data['capacity'],
+            availability = form.data['availability'],
+            host_id = current_user.id
+        )
+        db.session.add(spot)
+        db.session.commit()
+        return {"spot": {spot.id: spot.to_dict()}}
+    return {"errors": "set errors here"}
 
 
 @spot_routes.route('/<id>')
