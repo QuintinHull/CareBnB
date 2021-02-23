53b5522582980191020dded0efd84a96e979e2a1
Lane <lanedaltonjohns@gmail.com>
2021-02-23 18:11:49 -0500

    remove unused code

--- react-app/src/components/SpotComponent/SpotViewLarge/index.js
+++ react-app/src/components/SpotComponent/SpotViewLarge/index.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import React from "react"
 
 const SpotViewLarge = ({spot}) => {
     return (
