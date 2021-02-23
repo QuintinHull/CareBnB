a1ac1ffe925b2258300349fe8b3e580c06e29fb5
Mustafa <mustafaomousa@icloud.com>
2021-02-23 09:24:29 -0600

    setup the createnewspot in redux store

--- react-app/src/store/spot.js
+++ react-app/src/store/spot.js
@@ -1,4 +1,5 @@
 const LOAD = "spot/getAllSpots"
+const CREATE_SPOT = "spot/createNewSpot"
 
 const getAllSpots = (spots) => {
     return {
@@ -7,12 +8,41 @@ const getAllSpots = (spots) => {
     }
 }
 
+const createNewSpot = (spot) => {
+    return {
+        type: CREATE_SPOT,
+        payload: spot
+    }
+}
+
 export const getSpots = () => async dispatch => {
     const response = await fetch('/api/spots/');
-    console.log(response)
     const spots = await response.json()
     return dispatch(getAllSpots(spots));
 }
+
+export const createSpot = ({ image_url, title, address, city,
+    state, zipcode, description, capacity, availability }) => async dispatch => {
+        const response = await fetch('/api/spots/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                image_url,
+                title,
+                address,
+                city,
+                state,
+                zipcode,
+                description,
+                capacity,
+                availability
+            })
+        });
+        const spot = await response.json();
+        return dispatch(createNewSpot(spot))
+    }
 
 const initialState = {}
 
@@ -21,6 +51,11 @@ const spotReducer = (state = initialState, action) => {
     switch (action.type) {
         case LOAD:
             newState = Object.assign({}, state, { ...action.payload })
+            return newState
+        case CREATE_SPOT:
+            const new_spot = action.payload.spot
+            const all_spots = state.all_spots
+            newState = { all_spots: { ...all_spots, ...new_spot } }
             return newState
         default:
             return state
