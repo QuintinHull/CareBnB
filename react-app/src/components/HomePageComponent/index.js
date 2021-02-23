db547fb692ab0a45f9e698b7511844c036a4f430
Mustafa <mustafaomousa@icloud.com>
2021-02-23 15:18:35 -0600

    I created a backend route for fetch the 10 spots with the most availability

--- react-app/src/components/HomePageComponent/index.js
+++ react-app/src/components/HomePageComponent/index.js
@@ -1,26 +1,33 @@
 import React from "react"
 import './home-page.css'
 
+// const test = async () => {
+//     const response = await fetch('/api/spots/top-available');
+//     const spots = await response.json();
+//     console.log(spots)
+// }
+
 const HomePageComponent = () => {
     return (
         <div className='home-body'>
             <div className='welcome-search-container'>
                 <div className='home-search-bar-container'>
-                    <h4>Search Bar Component</h4>
+                    <h4>Search Bar Component with background image</h4>
                 </div>
-                <h4>Welcome background image</h4>
             </div>
             <div className='newest-spots-container'>
-                <h4>Loop over 10 newest spots and render SpotViewLarge components</h4>
+                <h4>Loop over 10 spots that have the highest availability</h4>
             </div>
             <div className='mission-container'>
-                <h4>Horizontal container with two inner containers</h4>
                 <div className='mission-statement-container'>
                     <h4>Mission statement</h4>
                 </div>
                 <div className='amount-sheltered-container'>
                     <h4>Amount of people who are sheltered</h4>
                 </div>
+            </div>
+            <div className='google-maps-container'>
+                <h4>Google Maps Here</h4>
             </div>
         </div>
     )
