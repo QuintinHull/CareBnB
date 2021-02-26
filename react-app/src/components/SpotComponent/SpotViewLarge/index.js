f1f6b6bc5b5b6e355e2354d639b2c1c886ea19b7
Jummies <jumpark1210@gmail.com>
2021-02-26 16:54:31 -0500

    spotedit

--- react-app/src/components/SpotComponent/SpotViewLarge/index.js
+++ react-app/src/components/SpotComponent/SpotViewLarge/index.js
@@ -1,13 +1,17 @@
 import React from "react"
+import './SpotViewLarge.css'
 
 const SpotViewLarge = ({ spot }) => {
     return (
         <div className="spot-view-large-body">
-            {/* <img src={spot.image_url} alt="Spot Image" /> */}
-            <h4>{spot.description}</h4>
+            
+
+            <h3>{spot.description}</h3>
             <h3>{spot.title}</h3>
             <h4>{spot.city}, {spot.state}</h4>
             <h4>{spot.availability}</h4>
+            <img src={spot.image_url} alt="Spot Image" className='spot-view-large-image' />
+
         </div>
     )
 }
