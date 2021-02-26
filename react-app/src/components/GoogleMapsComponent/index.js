322745a3151ae3e326754a6496cfe01050718c97
Lane <lanedaltonjohns@gmail.com>
2021-02-25 19:18:03 -0500

    work on getting marker to appear on google map

--- react-app/src/components/GoogleMapsComponent/index.js
+++ react-app/src/components/GoogleMapsComponent/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+import React, { useEffect } from 'react';
+import { useSelector , useDispatch} from 'react-redux'
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from 'react-google-maps';
 import Geolocation from '@react-native-community/geolocation';
 
+import { getSpots } from '../../store/spot'
+
+
 const GoogleMapsComponent = () => {
+
+    const allSpots = useSelector(state => state.all_spots)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getSpots())
+    }, [dispatch])
 
     return (
         <GoogleMap
             defaultZoom={10}
-            defaultCenter={{ lat: 32, lng: -97 }}
-        />
+            defaultCenter={{ lat: 32, lng: -97 }}>
+                {/* {allSpots && console.log("THIS SHOULD BE ALL SPOTS", allSpots)}
+                {allSpots && allSpots.map((spot) => (
+                    <Marker key={spot.id} position={{lat: 42.7978, lng: 83.7049}} />
+                ))} */}
+                <Marker key={1} position={{lat: 29, lng: 82}} />
+            </GoogleMap>
     )
 }
 
